87268476b4cafef703fc018249d1f401a06612bf
Your Name <sabavathnaveen951@gmail.com>
2023-02-13 16:39:35 +0530

    First Attempt of Pizza

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,23 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        super.flag_cheese = false;
+        super.flag_tops = false;
+        if(isVeg)
+        {
+            int cheesetops = 150;
+            setPrice(getPrice()+cheesetops);
+        }
+        else
+        {
+            int cheesetops =200;
+            setPrice(getPrice()+cheesetops);
+        }
+
         // your code goes here
     }
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,27 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    boolean flag_cheese = true;
+    boolean flag_tops = true;
+    boolean flag_paper = true;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg)
+        {
+            this.price = 300;
+        }
+        else {
+            this.price = 400;
+        }
         // your code goes here
+
     }
 
     public int getPrice(){
@@ -17,18 +34,52 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+
+       if(flag_cheese)
+       {
+           this.price +=80;
+           flag_cheese = false;
+       }
+
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(flag_tops)
+        {
+            if(isVeg)
+            {
+                this.price +=70;
+            }
+            else{
+                this.price += 120;
+            }
+            flag_tops = false;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+
+        if(flag_paper)
+        {
+            this.price +=20;
+            flag_paper = false;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(isVeg)
+        {
+            this.bill = "Base Price Of The Pizza: 300\nExtra Cheese Added: 80\nExtra Toppings Added: 70\nPaperbag Added: 20\nTotal Price: "+this.price;
+        }
+        else
+        {
+            this.bill = "Base Price Of The Pizza: 400\nExtra Cheese Added: 80\nExtra Toppings Added: 120\nPaperbag Added: 20\nTotal Price: "+this.price;
+        }
+
         return this.bill;
     }
 }
